a81f8e80b64af6309a75c8cc18b989c97c01fe95
Dominic Lai <dominiclai1983@gmail.com>
2023-08-02 20:11:38 +0200

    completed another test on todos.model.test.js

--- server/src/models/todos.model.test.js
+++ server/src/models/todos.model.test.js
@@ -1,4 +1,5 @@
 const mockingoose = require("mockingoose");
+const mongoose = require("mongoose");
 const todoModel = require("./todos.mongo");
 
 const {
@@ -12,8 +13,34 @@ const {
 } = require("./todos.model");
 
 describe("todo service", () => {
+  beforeEach(() => {
+    mockingoose.resetAll();
+  });
+
+  describe("postNewTodo", () => {
+    it.only("should return a new todo", async () => {
+      mockingoose(todoModel).toReturn(
+        {
+          //_id: ObjectId("64ca826d9bd20053a8ab675b"),
+          item: "Kiss my ass now!",
+          completed: false,
+          deleted: false,
+          user_id: 1001,
+        },
+        "save"
+      );
+      const user_id = 1001,
+        item = "Kiss my ass now!";
+
+      const result = await postNewTodo(user_id, item);
+
+      expect(result[0].id).toBeDefined();
+      expect(result[0].item).toBe("Kiss my ass now!");
+    });
+  });
+
   describe("getAllTodos", () => {
-    it("should return ths list of todo", async () => {
+    it("should return the list of todo", async () => {
       mockingoose(todoModel).toReturn(
         [
           {
